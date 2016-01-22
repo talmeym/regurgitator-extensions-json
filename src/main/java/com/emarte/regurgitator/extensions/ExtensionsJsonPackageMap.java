d3bfa7bc1f5e3b0e3de056245a3eb83516dc5f0f
talmeym <mtalmey@gmail.com>
2016-01-22 13:58:13 +0000

    tidied and sorted out XmlSchemaValidator tests

--- src/main/java/com/emarte/regurgitator/extensions/ExtensionsJsonPackageMap.java
+++ src/main/java/com/emarte/regurgitator/extensions/ExtensionsJsonPackageMap.java
@@ -5,7 +5,7 @@ import com.emarte.regurgitator.core.*;
 import java.util.*;
 
 public class ExtensionsJsonPackageMap extends AbstractJsonPackageMap {
-	private static final List<String> kinds = Arrays.asList("freemarker-builder", "xml-parameter", "json-parameter", "contains-json-path", "contains-xpath", "xpath-processor", "json-path-processor");
+	private static final List<String> kinds = Arrays.asList("freemarker-builder", "xml-parameter", "json-parameter", "contains-json-path", "contains-xpath", "xpath-processor", "json-path-processor", "xml-schema-validator");
 
 	public ExtensionsJsonPackageMap() {
 		addPackageMapping(kinds, "com.emarte.regurgitator.extensions");
