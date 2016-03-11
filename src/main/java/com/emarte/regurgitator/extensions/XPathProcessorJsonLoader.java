8f93fc2f96a02150feaf889a0cce363a0cf220a3
talmeym <mtalmey@gmail.com>
2016-03-11 09:21:40 +0000

    tidied logs

--- src/main/java/com/emarte/regurgitator/extensions/XPathProcessorJsonLoader.java
+++ src/main/java/com/emarte/regurgitator/extensions/XPathProcessorJsonLoader.java
@@ -6,11 +6,12 @@ import net.sf.json.JSONObject;
 import java.util.*;
 
 import static com.emarte.regurgitator.core.JsonConfigUtil.loadMandatoryStr;
+import static com.emarte.regurgitator.core.Log.getLog;
 import static com.emarte.regurgitator.extensions.ExtensionsConfigConstants.*;
 import static com.emarte.regurgitator.extensions.JsonNamespaceLoader.loadNamespaces;
 
 public class XpathProcessorJsonLoader implements JsonLoader<XpathProcessor> {
-	private static final Log log = Log.getLog(XpathProcessorJsonLoader.class);
+	private static final Log log = getLog(XpathProcessorJsonLoader.class);
 
 	@Override
 	public XpathProcessor load(JSONObject jsonObject, Set<Object> allIds) throws RegurgitatorException {
