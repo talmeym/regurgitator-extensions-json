09f3edf1970765aefa71c774247e3a266a699807
talmeym <mtalmey@gmail.com>
2017-09-05 08:43:24 +0100

    added missing json package mapping

--- src/main/java/com/emarte/regurgitator/extensions/ExtensionsJsonPackageMap.java
+++ src/main/java/com/emarte/regurgitator/extensions/ExtensionsJsonPackageMap.java
@@ -5,7 +5,7 @@ import com.emarte.regurgitator.core.AbstractJsonPackageMap;
 import java.util.*;
 
 public class ExtensionsJsonPackageMap extends AbstractJsonPackageMap {
-	private static final List<String> kinds = Arrays.asList("freemarker-builder", "velocity-builder", "xml-parameter", "json-parameter", "contains-json-path", "contains-xpath", "xpath-processor", "json-path-processor", "xml-schema-validator");
+	private static final List<String> kinds = Arrays.asList("freemarker-builder", "freemarker-processor", "velocity-builder", "velocity-processor", "xml-parameter", "json-parameter", "contains-json-path", "contains-xpath", "xpath-processor", "json-path-processor", "xml-schema-validator");
 
 	public ExtensionsJsonPackageMap() {
 		addPackageMapping(kinds, "com.emarte.regurgitator.extensions");
